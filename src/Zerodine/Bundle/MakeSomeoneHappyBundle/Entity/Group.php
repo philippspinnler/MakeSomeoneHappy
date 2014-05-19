51b1ba3da6744b8fd788cdeeb2c5260c02dfb556
Philipp Spinnler <philipp@spinnler.ch>
2014-05-19 16:47:57 +0200

    Created basic entities

---
+++ src/Zerodine/Bundle/MakeSomeoneHappyBundle/Entity/Group.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: philipp
+ * Date: 19/05/14
+ * Time: 16:22
+ */
+
+namespace Zerodine\Bundle\MakeSomeoneHappyBundle\Entity;
+
+
+class Group {
+    protected $id;
+
+    protected $name;
+
+    protected $alias;
+
+    protected $password;
+}
---
+++ src/Zerodine/Bundle/MakeSomeoneHappyBundle/Entity/Person.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Zerodine\Bundle\MakeSomeoneHappyBundle\Entity;
+
+
+class Person {
+    protected $id;
+
+    protected $name;
+
+    protected $child;
+
+    protected $keyPublic;
+
+    protected $keyPrivate;
+
+    protected $partner;
+
+    protected $group;
+} 
