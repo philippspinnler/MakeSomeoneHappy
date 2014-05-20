15a78a03a2db9bd1edfa714f9bc68c144bc73ff0
Philipp Spinnler <philipp@spinnler.ch>
2014-05-20 16:58:16 +0200

    Removed unneeded testing code

--- src/Zerodine/Bundle/MakeSomeoneHappyBundle/Resources/public/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('#loading-example-btn').click(function () {
-        var btn = $(this)
-        btn.button('loading')
-        setTimeout(function() {
-            btn.button("reset")
-        }, 1000);
-    });
-});
